0e746f0402e0e0c9d6ff8aefb73538ec8ff20bb8
ChermDev <carlos_e8246@hotmail.com>
2022-12-09 19:14:25 -0600

    view, urls, templates and debug toolbar

--- storefront/settings.py
+++ storefront/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "playground"
+    "debug_toolbar",
+    "playground",
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "storefront.urls"
